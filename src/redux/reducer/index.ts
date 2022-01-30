8280e11a851c425182370d2c24e325c61cf1bf17
Chee Chong <cheechongtey@gmail.com>
2022-01-30 08:12:26 +0800

    Fix production bug

--- src/redux/reducer/index.ts
+++ src/redux/reducer/index.ts
@@ -1,8 +1,5 @@
 import { combineReducers } from "redux"
-import navbarReducer from "./navbar"
 
-const rootReducer = combineReducers({
-    navbar: navbarReducer,
-})
+const rootReducer = combineReducers({})
 
 export default rootReducer
